bf36d85cc6b98faebc4755e315bdd7f9d1310231
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-03-03 23:03:54 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+var scene1 = document.getElementById('scene1');
+var parallaxInstance = new Parallax(scene);
